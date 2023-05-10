4244d92c06fb8fa07aeee5c71f3d9e094141e732
unknown <y2643@lgcns.com>
2023-05-10 15:07:57 +0900

    2-3

--- minibank_account/src/main/java/com/minibank/account/AccountApplication.java
+++ minibank_account/src/main/java/com/minibank/account/AccountApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 //TODO : EurekaClient 활성 어노테이션 추가
-
+@EnableEurekaClient
 @SpringBootApplication
 public class AccountApplication {
 	public static void main(String[] args) {
--- minibank_account/src/main/java/com/minibank/account/rest/customer/CustomerRestClient.java
+++ minibank_account/src/main/java/com/minibank/account/rest/customer/CustomerRestClient.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
@@ -21,6 +22,7 @@ public class CustomerRestClient {
     @Value("${customer.api.url}")
     private String CUSTOMER_API_URL;
     
+    //TODO: RestTemplate에 Spring Cloud Loadbalancer 적용
     @Bean
     RestTemplate getRestTemplate() {
         return new RestTemplate();
@@ -36,7 +38,6 @@ public class CustomerRestClient {
         return this.restTemplate.getForObject(CUSTOMER_API_URL + apiUrl, Customer.class, cstmId);
 	}
     
-	//TODO : fallback method 추가
     public Customer fallbackRetrieveCustomer(String cstmId, Throwable t) throws Exception {
     	String msg = "restTemplate를 이용하여 " + cstmId + "고객정보 조회 서비스 호출에 문제가 있습니다.";
     	LOGGER.error(msg, t);
